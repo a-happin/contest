6725af09b224d6d1a9e38a9ba9ab10b88838efb1
anyumekawa <10003601+a@users.noreply.github.com>
2019-08-09 19:16:42 +0900

    Fix template.cpp

--- template.cpp
+++ template.cpp
@@ -1,14 +1,4 @@
 #include <bits/stdc++.h>
-
-#ifdef LOCAL
-  #include <contest/local.hpp>
-#else
-  #define LOG(...) do {} while (false)
-#endif
-
-#define REP(i,n) for (decay_t <decltype (n)> i = 0, i ## __len = (n); i < i ## __len; ++ i)
-#define ALL(x) (x).begin (), (x).end ()
-
 using namespace std;
 
 auto main () -> int
@@ -16,6 +6,6 @@ auto main () -> int
   cin.tie (nullptr);
   ios::sync_with_stdio (false);
 
-
+  
 }
 
